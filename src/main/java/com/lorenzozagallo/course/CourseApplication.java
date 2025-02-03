c7328dc34ce9a0e862b41b5e2491d4b9f74884fe
Bowie <marketing@letscode.com.br>
2025-02-03 11:00:18 -0300

    h2 database, test profile, JPA

---
+++ src/main/java/com/lorenzozagallo/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.lorenzozagallo.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // annotation
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
