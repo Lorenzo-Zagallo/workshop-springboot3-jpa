eb87b5cb79a39ee034c4abc97703d07f2d666a3d
Bowie <marketing@letscode.com.br>
2025-02-03 15:50:00 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/lorenzozagallo/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lorenzozagallo.course.repositories;
+
+import com.lorenzozagallo.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
