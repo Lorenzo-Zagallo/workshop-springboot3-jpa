915595cdd11b9fd008f050ddbca3dbae617dc7dd
Bowie <marketing@letscode.com.br>
2025-02-03 11:13:24 -0300

    test

--- src/main/java/com/lorenzozagallo/course/entities/User.java
+++ src/main/java/com/lorenzozagallo/course/entities/User.java
@@ -18,6 +18,7 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @Column
     private String name;
 
     private String email;
