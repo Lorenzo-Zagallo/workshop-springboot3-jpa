27f55bf785791c1809b4b673d31f2727d8c478d6
Bowie <marketing@letscode.com.br>
2025-02-03 16:56:40 -0300

    service layer, component registration

--- src/main/java/com/lorenzozagallo/course/resources/UserResource.java
+++ src/main/java/com/lorenzozagallo/course/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.lorenzozagallo.course.resources;
 
 import com.lorenzozagallo.course.entities.User;
+import com.lorenzozagallo.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
+    // requisição do tipo GET
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(UUID.randomUUID(), "Maria", "maria@gmail.com", "9999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+//        User u = new User(UUID.randomUUID(), "Maria", "maria@gmail.com", "9999999", "12345");
+//        return ResponseEntity.ok().body(u);
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable UUID id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/lorenzozagallo/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.lorenzozagallo.course.services;
+
+import com.lorenzozagallo.course.entities.User;
+import com.lorenzozagallo.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    // método para retornar todos os usuários do banco de dados
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    // achar um usuário por ID
+    public User findById(UUID id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
