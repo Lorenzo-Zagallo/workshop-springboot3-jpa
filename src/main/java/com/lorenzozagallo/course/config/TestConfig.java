42817ea64555107d119673bd7f593a6069c1b27b
Bowie <marketing@letscode.com.br>
2025-02-04 00:35:12 -0300

    orderStatus enum

--- src/main/java/com/lorenzozagallo/course/config/TestConfig.java
+++ src/main/java/com/lorenzozagallo/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.lorenzozagallo.course.config;
 
 import com.lorenzozagallo.course.entities.Order;
 import com.lorenzozagallo.course.entities.User;
+import com.lorenzozagallo.course.entities.enums.OrderStatus;
 import com.lorenzozagallo.course.repositories.OrderRepository;
 import com.lorenzozagallo.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2024-11-20T19:53:07.000-03:00"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-12-21T03:42:10.000-03:00"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-12-22T15:21:22.000-03:00"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-11-20T19:53:07.000-03:00"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2024-12-21T03:42:10.000-03:00"), OrderStatus.WAITING_PAYMENT,u2);
+        Order o3 = new Order(null, Instant.parse("2024-12-22T15:21:22.000-03:00"), OrderStatus.CANCELED,u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
